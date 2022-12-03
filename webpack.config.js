7e4365769e6e70e00e9b562d19790d4cc5987de8
Andrey Prokhorov <beta737@yandex.ru>
2022-12-03 20:59:46 +0300

    project iPhone13Pro

---
+++ webpack.config.js
@@ -0,0 +1,113 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+
+const mode = process.env.NODE_ENV || 'development'
+const devMode = mode === 'development'
+const target = devMode ? 'web' : 'browserslist'
+const devtool = devMode ? 'source-map' : undefined
+
+
+
+module.exports = {
+    mode,
+    target,
+    devtool,
+    devServer: {
+        port: 4500,
+        open: true,
+        hot: true
+    },
+    entry: ['@babel/polyfill', path.resolve(__dirname, 'src', 'index.js')],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: '[name].[contenthash].js',
+        assetModuleFilename: 'assets/[name][ext]'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: '',
+            template: path.resolve(__dirname, 'src', 'index.html')
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(c|sa|sc)ss$/i,
+                use: [
+                    devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')]
+                            } 
+                        }
+                    },
+                    "sass-loader"
+                ],
+            },
+            {
+                test: /\.(ttf|woff|woff2|eot)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'fonts/[name][ext]'
+                },
+                
+            },
+            {
+                test: /\.(png|jpe?g|svg|gif|webp)$/i,
+                use: [
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                              progressive: true,
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                              enabled: false,
+                            },
+                            pngquant: {
+                              quality: '65-90',
+                              speed: 4
+                            },
+                            gifsicle: {
+                              interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                              quality: 75
+                            },
+                            svgo:{
+                                enabled: false,
+                            }
+                        }
+                    }
+                ],
+                type: 'asset/resource',
+                
+            },
+            {
+                test: /\.m?js$/i,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    }
+};
